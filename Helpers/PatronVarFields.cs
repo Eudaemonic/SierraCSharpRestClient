0c1c3ae64338f4fc745cec45342e91edbed9c755
Steven Baker <stevenbaker76@live.co.uk>
2021-10-07 17:21:23 +0100

    Added VarfField dictionary

---
+++ Helpers/PatronVarFields.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace SierraCSharpRestClient.Helpers
+{
+    public class PatronVarFields
+    {
+        public string GetVarFieldName(string key)
+        {
+
+
+            var dict = new Dictionary<string, string>
+            {
+                { "n", "name" },
+                { "b", "barcode" },
+                { "m", "message" },
+                { "u", "university id" },
+                { "z", "email" },
+                { "43", "expiry date" },
+                { "i", "institution" },
+                { "d", "department" },
+                { "j", "subject" },
+                { "x", "note" }
+            };
+
+            if (dict.ContainsKey(key.Trim()))
+            {
+                return dict[key];
+            }
+
+            return key;
+
+
+        }
+
+        
+
+    }
+}
